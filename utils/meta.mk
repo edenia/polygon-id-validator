7119bc9b2031518bf964ff5ff3053c02b2fba592
Leister Francisco Alvarez Campos <leisterac.1997@gmail.com>
2023-09-13 07:01:48 -0600

    cicd(general): update deployment variables

--- utils/meta.mk
+++ utils/meta.mk
@@ -6,13 +6,12 @@ CURRENT_BRANCH ?= $(shell git rev-parse --abbrev-ref HEAD)
 IMAGE_NAME_WEBAPP=webapp
 IMAGE_NAME_HAPI=hapi
 IMAGE_NAME_HASURA=hasura
-IMAGE_NAME_WALLET=wallet
 
 IMAGE_ID=$(shell docker images --format '{{.ID}}' --filter reference='docker.pkg.github.com/edenia/edenia.com*latest')
 DOCKER_REGISTRY=ghcr.io/edenia/boilerplate
-SUBDIRS = webapp hapi hasura wallet
+SUBDIRS = webapp hapi hasura
 
-MAKE_ENV += DOCKER_REGISTRY VERSION IMAGE_NAME_WEBAPP IMAGE_NAME_HAPI IMAGE_NAME_WALLET IMAGE_NAME_HASURA
+MAKE_ENV += DOCKER_REGISTRY VERSION IMAGE_NAME_WEBAPP IMAGE_NAME_HAPI IMAGE_NAME_HASURA
 
 SHELL_EXPORT := $(foreach v,$(MAKE_ENV),$(v)='$($(v))')
 
